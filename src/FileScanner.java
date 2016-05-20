32176c5730efae0756b306188ec1d5935ae83a92
Flexuss <flexusssss@gmail.com>
2016-05-20 22:31:12 +0300

    Update FileScanner.java

--- src/FileScanner.java
+++ src/FileScanner.java
@@ -4,36 +4,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileScanner {
-
+	//Сканирование директории
 	static void scan(String iD, String oD, String mask, boolean inSubfold, boolean aDel) throws IOException, InterruptedException {
 		File[] input = new File(iD).listFiles();
-		System.out.println("�������� ����������");
+		System.out.println("File search");
 		for (int i = 0; i < input.length; i++) {
 			if (input[i].isFile()) {
-				if (chek(input[i], mask)) {
+				if (chek(input[i], mask)) {//Если совпадает с маской создает новый файл
 					File output = new File(oD, input[i].getName());
 					output.createNewFile();
 					if (aDel) {
-						input[i].delete();
+						input[i].delete();//Если требуется, удаляет файл
 					}
 				}
 			} else {
-				if (input[i].isDirectory() && inSubfold) {
+				if (input[i].isDirectory() && inSubfold) {//Если требуется, сканирует поддиректории
 					File dir = new File(oD, input[i].getName());
 					String iD1 = iD + "\\\\" + dir.getName();
 					String oD1 = oD + "\\\\" + dir.getName();
-					System.out.println(iD1);
-					System.out.println(oD1);
 					dir.mkdir();
-					System.out.println("������ ����������");
+					System.out.println("Create subfolder "+oD1)
+					System.out.println("Scan subfolder "+iD1);
 					scan(iD1, oD1, mask, inSubfold, aDel);
-				} else {
-					System.out.println("���");
-				}
+				} 
 			}
 		}
 	}
-
+	//Проверка файла по маске
 	private static boolean chek(File a, String mask) {
 		Pattern p = Pattern.compile(mask);
 		Matcher m = p.matcher(a.getName());
